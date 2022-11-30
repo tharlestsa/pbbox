75ce7b04fbe3ebe32a6ba3f2323bd0da16cfb296
Tharles de Sousa Andrade <irtharles@gmail.com>
2022-11-30 00:05:31 -0300

    Fix some errros

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Command to create a bbox from a point.
 
 ## Installation
 pip install git+https://github.com/tharlestsa/point-bbox
+
+sudo ln -s <path_to_project>/point_bbox/point_bbox/point_bbox.py /usr/bin/bbox
--- point_bbox/point_bbox.py
+++ point_bbox/point_bbox.py
@@ -1,34 +1,36 @@
+#! /usr/bin/env python3
+
 import argparse
 from pyproj import Transformer
 from shapely.geometry import Point
 
-parser = argparse.ArgumentParser(
-    fromfile_prefix_chars='@',
-    description='Create a bbox from point',
-    epilog='Enjoy the program! :)'
-)
+try:
 
-parser.add_argument('--lon', type=float, help='Longitude of the point')
+    parser = argparse.ArgumentParser(
+        fromfile_prefix_chars='@',
+        description='Create a bbox from point',
+        epilog='Enjoy the program! :)'
+    )
 
-parser.add_argument('--lat', type=float, help='Latitude of the point')
+    parser.add_argument('--lon', type=float, help='Longitude of the point')
 
-parser.add_argument('--crs_from', type=str,  nargs='?', default='epsg:4326', help="Code of projection ex. 'epsg:4326'")
+    parser.add_argument('--lat', type=float, help='Latitude of the point')
 
-parser.add_argument('--crs_to', type=str,  nargs='?', default='epsg:900913', help="Code of projection ex. 'epsg:900913'")
+    parser.add_argument('--crs_from', type=str,  nargs='?', default='epsg:4326', help="Code of projection default: 'epsg:4326'")
 
-parser.add_argument('--size', default=4000,  nargs='?', type=int, help='Size of the bbox in meters')
+    parser.add_argument('--crs_to', type=str,  nargs='?', default='epsg:900913', help="Code of projection default: 'epsg:900913'")
 
-def calc_bbox():
+    parser.add_argument('--size', default=4000,  nargs='?', type=int, help='Size of the bbox in meters default: 4000')
+
     # Execute parse_args()
     args = parser.parse_args()
-    # "epsg:4326", "epsg:900913"
-    crs_from = args.crs_from
-    crs_to = args.crs_to
-    transformer = Transformer.from_crs(crs_from, crs_to, always_xy=True)
+    
+    transformer = Transformer.from_crs(args.crs_from, args.crs_to, always_xy=True)
     x, y = transformer.transform(args.lon, args.lat)
     buffer = Point(x, y).buffer(args.size)
-    return buffer.bounds
+    bbox = buffer.bounds
 
-if __name__ == "__main__":
-    bbox = calc_bbox()
     print(f"{bbox[0]},{bbox[1]},{bbox[2]},{bbox[3]}")
+
+except Exception as e:
+    print("Error: ", e)
